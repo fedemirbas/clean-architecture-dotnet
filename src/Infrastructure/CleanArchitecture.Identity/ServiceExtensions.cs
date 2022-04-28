82a85752440e9b2b8cc93956321d388657a3596d
fatih emre demirbaş <f.emredemirbas@gmail.com>
2022-04-29 00:24:04 +0300

    bug fixed

--- src/Infrastructure/CleanArchitecture.Identity/ServiceExtensions.cs
+++ src/Infrastructure/CleanArchitecture.Identity/ServiceExtensions.cs
@@ -1,6 +1,15 @@
 ﻿using System;
+using System.Text;
+using System.Text.Json;
+using CleanArchitecture.Application.Interfaces.Services;
+using CleanArchitecture.Application.Wrappers;
+using CleanArchitecture.Domain.Settings;
+using CleanArchitecture.Identity.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CleanArchitecture.Identity
 {
@@ -8,7 +17,57 @@ namespace CleanArchitecture.Identity
     {
         public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            
+            #region Services
+            services.AddTransient<IUserService, UserService>();
+            #endregion
+
+            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JWTSettings:Issuer"],
+                        ValidAudience = configuration["JWTSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+                    };
+                    o.Events = new JwtBearerEvents()
+                    {
+                        OnAuthenticationFailed = c =>
+                        {
+                            c.NoResult();
+                            c.Response.StatusCode = 500;
+                            c.Response.ContentType = "text/plain";
+                            return c.Response.WriteAsync(c.Exception.ToString());
+                        },
+                        OnChallenge = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.StatusCode = 401;
+                            context.Response.ContentType = "application/json";
+                            var result = JsonSerializer.Serialize(new Response<string>("You are not Authorized"));
+                            return context.Response.WriteAsync(result);
+                        },
+                        OnForbidden = context =>
+                        {
+                            context.Response.StatusCode = 403;
+                            context.Response.ContentType = "application/json";
+                            var result = JsonSerializer.Serialize(new Response<string>("You are not authorized to access this resource"));
+                            return context.Response.WriteAsync(result);
+                        },
+                    };
+                });
         }
     }
 }
